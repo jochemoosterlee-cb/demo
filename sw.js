3b0e984024234c9e88285b5a7b568a086d746e12
Jochem Oosterlee <jochem.oosterlee@cleverbase.com>
2025-10-16 09:24:32 +0200

    update

--- sw.js
+++ sw.js
@@ -4,8 +4,17 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         './',
         './index.html',
+        './portal.html',
         './manifest.json',
-        './icon.svg'
+        './icon.svg',
+        './line.svg',
+        './eidas.svg',
+        './checkmark.svg',
+        'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js',
+        'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js',
+        'https://cdn.tailwindcss.com',
+        'https://unpkg.com/html5-qrcode@2.3.8/dist/html5-qrcode.min.js',
+        'https://cdn.jsdelivr.net/npm/qrcode@1.5.3/build/qrcode.min.js'
       ]);
     })
   );
